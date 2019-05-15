80d7bc78eaf91c7e5f9625b4a2ea1ad1bd308048
Pouya <pouyaheydary@gmail.com>
2019-05-15 20:28:02 +0430

    end of session 10

--- app/src/main/java/com/sematec/bootcamp/eight/androidadvancedfarvardin98/features/mvvm/TestMVVMActivity.kt
+++ app/src/main/java/com/sematec/bootcamp/eight/androidadvancedfarvardin98/features/mvvm/TestMVVMActivity.kt
@@ -2,15 +2,26 @@ package com.sematec.bootcamp.eight.androidadvancedfarvardin98.features.mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import com.sematec.bootcamp.eight.androidadvancedfarvardin98.R
+import kotlinx.android.synthetic.main.activity_test_mvvm.*
 
 class TestMVVMActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_mvvm)
-
-
         val viewModel = TestViewModel()
+
+        btn.setOnClickListener {
+            viewModel.getUser()
+        }
+
+
+        //Observer
+        viewModel.observable.observe(this, Observer {
+            Toast.makeText(this@TestMVVMActivity, it, Toast.LENGTH_LONG).show()
+        })
     }
 }
--- app/src/main/java/com/sematec/bootcamp/eight/androidadvancedfarvardin98/features/mvvm/TestViewModel.kt
+++ app/src/main/java/com/sematec/bootcamp/eight/androidadvancedfarvardin98/features/mvvm/TestViewModel.kt
@@ -2,9 +2,16 @@ package com.sematec.bootcamp.eight.androidadvancedfarvardin98.features.mvvm
 
 import androidx.lifecycle.MutableLiveData
 
+
+//ViewModel
 class TestViewModel {
 
 
+    //Observable
+    val observable = MutableLiveData<String>()
 
-    val data = MutableLiveData<String>()
+
+    fun getUser() {
+        observable.value = "Pouya"
+    }
 }
